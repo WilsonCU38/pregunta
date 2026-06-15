8133a3fb921cd34b08639baa6b92dd5a478e6084
Chicaiza U. Wilson <wilsoncu38@uniandes.edu.ec>
2026-06-14 21:23:15 -0500

    Semana 5 - Tarea 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pregunta } from './pregunta/pregunta';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Pregunta],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/pregunta/pregunta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pregunta',
+  imports: [],
+  templateUrl: './pregunta.html',
+  styleUrl: './pregunta.css',
+})
+export class Pregunta {}
